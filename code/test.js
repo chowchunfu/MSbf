953149775ea7157a5b84c1e69a75562b297f575c
chowchunfu <77260012+chowchunfu@users.noreply.github.com>
2024-05-20 14:35:36 +0800

    Add files via upload

--- code/test.js
+++ code/test.js
@@ -1,1 +1,10 @@
+NUM = 9
+
+function fn(){
+	const div1 = document.getElementById("div1");
+	div1.textContent = "button clicked";
+	
+	const p1 = document.getElementById("p1");
+	p1.textContent = "osdf";
+	}
 
